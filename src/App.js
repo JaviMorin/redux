c18f8b0b3331867304ea49e5f31b7f5924e39c95
JaviMorin <javiermc_23@hotmail.es>
2023-01-17 23:07:07 +0100

    Funciona aunque no esté perfecto.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { createStore } from 'redux'
 
 const counterReducer = (state = 0, action) => {
@@ -43,7 +43,8 @@ const App = () => {
 }
 
 const renderApp = () => {
-  ReactDOM.render(<App />, document.getElementById('root'))
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(<App />);
 }
 
 renderApp()
